e814a254b8edba44d6cfbf25c5a5ff9f547e21b3
Shapo <ShaboJia@gmail.com>
2023-06-23 16:05:57 +0800

    split result of task_handler to multiple html files

--- src/task_handler.rs
+++ src/task_handler.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::ops::RangeInclusive;
 use std::time::Duration;
 
@@ -8,18 +7,17 @@ use log::{debug, info};
 use tokio::time::sleep;
 
 use crate::config::Config;
-use crate::exporter::{Exporter, HTMLPage, Picture};
-use crate::html_generator::HTMLGenerator;
+use crate::data::Posts;
+use crate::exporter::Exporter;
 use crate::persister::Persister;
 use crate::post_processor::PostProcessor;
 use crate::resource_manager::ResourceManager;
-use crate::utils::pic_url_to_file;
 use crate::web_fetcher::WebFetcher;
+
+const SAVING_PERIOD: usize = 200;
 
 #[derive(Debug)]
 pub struct TaskHandler {
-    resource_manager: ResourceManager,
-    generator: HTMLGenerator,
     exporter: Exporter,
     config: Config,
     processer: PostProcessor,
@@ -33,49 +31,55 @@ impl TaskHandler {
         );
         let persister = Persister::build(&config.db).await?;
         let resource_manager = ResourceManager::build(fetcher, persister);
-        let emoticon = resource_manager.get_emoticon().await?;
         Ok(TaskHandler {
-            resource_manager,
-            generator: HTMLGenerator::new(),
             exporter: Exporter::new(),
             config,
-            processer: PostProcessor::new(emoticon),
+            processer: PostProcessor::build(resource_manager).await?,
         })
     }
 
-    pub async fn download(&self, range: RangeInclusive<u64>) -> Result<()> {
+    #[allow(unused)]
+    pub async fn download_meta_only(&self, range: RangeInclusive<u64>) -> Result<()> {
         self.download_posts(range, false, false).await
     }
 
+    #[allow(unused)]
     pub async fn download_with_pic(&self, range: RangeInclusive<u64>) -> Result<()> {
         self.download_posts(range, true, false).await
     }
 
-    pub async fn download_with_pic_and_export(&self, range: RangeInclusive<u64>) -> Result<()> {
+    pub async fn export_from_net(&self, range: RangeInclusive<u64>) -> Result<()> {
         self.download_posts(range, true, true).await
+    }
+
+    #[allow(unused)]
+    pub async fn export_from_local(
+        &self,
+        _range: RangeInclusive<u64>,
+        _reverse: bool,
+    ) -> Result<()> {
+        todo!()
     }
 
     async fn download_posts(
         &self,
-        mut range: RangeInclusive<u64>,
+        range: RangeInclusive<u64>,
         with_pic: bool,
         export: bool,
     ) -> Result<()> {
         let task_name = format!("weiback-{}", chrono::Local::now().format("%F-%R"));
+        let target_dir = std::env::current_dir()?.join(task_name);
 
-        if range.start() == &0 {
-            range = RangeInclusive::new(1, *range.end());
-        }
+        assert!(range.start() != &0);
         info!("pages download range is {range:?}");
         let mut total_posts_sum = 0;
-        let mut pic_to_fetch: HashSet<String> = HashSet::new();
-        let mut html = String::new();
-        for page in range {
-            let posts_meta = self
-                .resource_manager
+        let mut posts_acc = Posts::new();
+        for (i, page) in range.enumerate() {
+            let posts = self
+                .processer
                 .get_fav_posts_from_web(self.config.uid.as_str(), page)
                 .await?;
-            let posts_sum = posts_meta.len();
+            let posts_sum = posts.len();
             total_posts_sum += posts_sum;
             debug!("fetched {} posts in {}th page", posts_sum, page);
             if posts_sum == 0 {
@@ -83,29 +87,25 @@ impl TaskHandler {
                 break;
             }
 
-            if with_pic {
-                for mut post in posts_meta {
-                    self.processer.process_post(&mut post, &mut pic_to_fetch)?;
-                    if export {
-                        html.push_str(self.generator.generate_post(post).await?.as_str());
-                    }
+            if with_pic && !export {
+                self.processer.save_post_pictures(posts).await?
+            } else if export {
+                posts_acc.append(posts);
+                if i % SAVING_PERIOD == SAVING_PERIOD - 1 || posts_sum == 0 {
+                    let subtask_name = format!("weiback-{}", page);
+                    self.exporter
+                        .export_page(
+                            &subtask_name,
+                            self.processer
+                                .generate_html(posts_acc, &subtask_name)
+                                .await?,
+                            &target_dir,
+                        )
+                        .await?;
+                    posts_acc = Posts::new();
                 }
             }
             sleep(Duration::from_secs(5)).await;
-        }
-
-        if export {
-            let mut pics = Vec::new();
-            for pic_url in pic_to_fetch {
-                let name = pic_url_to_file(&pic_url).into();
-                let blob = self.resource_manager.get_pic(&pic_url).await?;
-                pics.push(Picture { name, blob });
-            }
-            let html = self.generator.generate_page(&html).await?;
-            let page = HTMLPage { html, pics };
-            self.exporter
-                .export_page(task_name, page, std::path::PathBuf::from("./").as_path())
-                .await?;
         }
         info!("fetched {total_posts_sum} posts in total");
         Ok(())
