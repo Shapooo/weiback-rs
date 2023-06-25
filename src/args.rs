59dc840988f8da254ad967df54c48720d025d04a
shapooo <shabojia@outlook.com>
2023-06-25 17:14:13 +0800

    remove db path from cli args

--- src/args.rs
+++ src/args.rs
@@ -1,12 +1,10 @@
-use clap::Parser;
 use std::path::PathBuf;
+
+use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about)]
 pub struct Args {
     /// location of config file
     #[arg(short, long)]
     pub config: Option<PathBuf>,
-    /// location of database file
-    #[arg(short, long)]
-    pub db: Option<String>,
 }
