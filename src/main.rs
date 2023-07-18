389c5ee3c52ca040c8d64d1ef81d5d79649decc0
shapooo <shabojia@outlook.com>
2023-07-18 17:51:14 +0800

    bugfix: put windows cfg_attr to root

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 use anyhow::Result;
 use env_logger::Builder;
 use log::{info, LevelFilter};
