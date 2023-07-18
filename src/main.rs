3e466555a17104c97cfb683b882c911b761e5401
shapooo <shabojia@outlook.com>
2023-07-18 15:30:59 +0800

    replace simple_logger with env_logger, for writing log to file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use anyhow::Result;
-use log::info;
-use simple_logger::SimpleLogger;
+use env_logger::Builder;
+use log::{info, LevelFilter};
 
 use weiback_rs::core::Core;
 
 fn main() -> Result<()> {
-    SimpleLogger::new()
-        .with_colors(true)
-        .with_level(log::LevelFilter::Warn)
-        .with_module_level("sqlx", log::LevelFilter::Error)
-        .env()
-        .init()
-        .unwrap();
+    let log_path = std::env::current_exe()?;
+    let log_path = log_path
+        .parent()
+        .ok_or(anyhow::anyhow!(
+            "the executable: {:?} should have parent, maybe bugs in there",
+            std::env::current_exe()
+        ))?
+        .join("res/weiback.log");
+    let log_file = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(log_path)?;
+    Builder::new()
+        .filter_level(LevelFilter::Warn)
+        .parse_default_env()
+        .filter_module("sqlx", LevelFilter::Error)
+        .target(env_logger::Target::Pipe(Box::new(log_file)))
+        .init();
     info!("start running...");
     let core = Core::new();
     core.run()?;
