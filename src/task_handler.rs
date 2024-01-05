c86e834e87631c073050e704b1cfcc2b0fdf4ac5
shapooo <shabojia@outlook.com>
2024-01-05 19:46:10 +0800

    refactor task_handler, call Post methods directly
    Former-commit-id: 2bb28591483d52a8e0f231729ca399ccc06d6be8

--- src/task_handler.rs
+++ src/task_handler.rs
@@ -1,24 +1,29 @@
+use crate::{
+    emoticon::init_emoticon,
+    error::{Error, Result},
+    exporter::Exporter,
+    login::LoginInfo,
+    message::TaskStatus,
+    persister::Persister,
+    post::Post,
+    user::User,
+    web_fetcher::WebFetcher,
+};
+
 use std::ops::RangeInclusive;
 use std::sync::{Arc, RwLock};
 use std::time::Duration;
 
-use log::{error, info};
+use futures::future::join_all;
+use log::{debug, error, info};
 use tokio::time::sleep;
-
-use crate::error::{Error, Result};
-use crate::exporter::Exporter;
-use crate::login::LoginInfo;
-use crate::message::TaskStatus;
-use crate::persister::Persister;
-use crate::post_processor::PostProcessor;
-use crate::web_fetcher::WebFetcher;
 
 const SAVING_PERIOD: usize = 200;
 
 #[derive(Debug)]
 pub struct TaskHandler {
-    exporter: Exporter,
-    processer: PostProcessor,
+    web_fetcher: WebFetcher,
+    persister: Persister,
     task_status: Arc<RwLock<TaskStatus>>,
     uid: i64,
 }
@@ -33,39 +38,37 @@ impl TaskHandler {
                 login_info
             )));
         };
-        let fetcher =
+        let web_fetcher =
             WebFetcher::from_cookies(uid, serde_json::from_value(login_info["cookies"].take())?)?;
         let persister = Persister::new();
 
         Ok(TaskHandler {
-            exporter: Exporter::new(),
-            processer: PostProcessor::new(fetcher, persister),
+            web_fetcher,
+            persister,
             task_status,
             uid,
         })
     }
 
     pub async fn init(&mut self) -> Result<()> {
-        self.processer.init().await?;
-        let (web_total, db_total) = tokio::join!(
-            self.processer.get_web_total_num(),
-            self.processer.get_db_total_num()
-        );
+        let (web_total, db_total) = tokio::join!(self.get_web_total_num(), self.get_db_total_num());
         let web_total = web_total?;
+        debug!("initing...");
+        init_emoticon(&self.web_fetcher).await?;
+        self.persister.init().await?;
         *self.task_status.write().unwrap() = TaskStatus::Init(web_total, db_total?);
         Ok(())
     }
 
-    pub async fn unfavorite_posts(&self, range: RangeInclusive<u32>) {
-        self.handle_task_res(self._unfavorite_posts(range).await)
-            .await
+    pub async fn unfavorite_posts(&self) {
+        self.handle_task_res(self._unfavorite_posts().await).await
     }
 
-    async fn _unfavorite_posts(&self, range: RangeInclusive<u32>) -> Result<()> {
-        let ids = self.processer.get_fav_ids_to_unfavorite(range).await?;
+    async fn _unfavorite_posts(&self) -> Result<()> {
+        let ids = Post::query_posts_to_unfavorite(self.persister.db().unwrap()).await?;
         let len = ids.len();
         for (i, id) in ids.into_iter().enumerate() {
-            self.processer.unfavorite_post(id).await?;
+            Post::unfavorite_post(id, self.persister.db().unwrap(), &self.web_fetcher).await?;
             info!("post {id} unfavorited");
             tokio::time::sleep(std::time::Duration::from_secs(1)).await;
             let _ = self.task_status.try_write().map(|mut op| {
@@ -82,18 +85,7 @@ impl TaskHandler {
         with_pic: bool,
         image_definition: u8,
     ) {
-        self.handle_task_res(self._backup_self(range, with_pic, image_definition).await)
-            .await
-    }
-
-    async fn _backup_self(
-        &self,
-        range: RangeInclusive<u32>,
-        with_pic: bool,
-        image_definition: u8,
-    ) -> Result<()> {
-        let uid = self.uid;
-        self._backup_user(uid, range, with_pic, image_definition)
+        self.backup_user(self.uid, range, with_pic, image_definition)
             .await
     }
 
@@ -121,8 +113,7 @@ impl TaskHandler {
         assert!(range.start() != &0);
         info!("download user {uid} posts, range is {range:?}");
         for page in range {
-            self.processer
-                .backup_ones_posts(uid, page, with_pic, image_definition)
+            self.backup_ones_posts(uid, page, with_pic, image_definition)
                 .await?;
         }
         Ok(())
@@ -151,10 +142,7 @@ impl TaskHandler {
         let task_name = format!("weiback-{}", chrono::Local::now().format("%F-%H-%M"));
         let target_dir = std::env::current_dir()?.join(task_name);
 
-        let mut local_posts = self
-            .processer
-            .load_fav_posts_from_db(range, reverse)
-            .await?;
+        let mut local_posts = self.load_fav_posts_from_db(range, reverse).await?;
         let posts_sum = local_posts.len();
         info!("fetched {} posts from local", posts_sum);
 
@@ -162,26 +150,26 @@ impl TaskHandler {
         loop {
             let subtask_name = format!("weiback-{index}");
             if local_posts.len() < SAVING_PERIOD {
-                let html = self
-                    .processer
-                    .generate_html(local_posts, &subtask_name, image_definition)
-                    .await?;
-                self.exporter
-                    .export_page(&subtask_name, html, &target_dir)
-                    .await?;
+                let html = Post::generate_html(
+                    local_posts,
+                    &subtask_name,
+                    image_definition,
+                    self.persister.db().unwrap(),
+                    &self.web_fetcher,
+                )
+                .await?;
+                Exporter::export_page(&subtask_name, html, &target_dir).await?;
                 break;
             } else {
-                let html = self
-                    .processer
-                    .generate_html(
-                        local_posts.split_off(SAVING_PERIOD),
-                        &subtask_name,
-                        image_definition,
-                    )
-                    .await?;
-                self.exporter
-                    .export_page(&subtask_name, html, &target_dir)
-                    .await?;
+                let html = Post::generate_html(
+                    local_posts.split_off(SAVING_PERIOD),
+                    &subtask_name,
+                    image_definition,
+                    self.persister.db().unwrap(),
+                    &self.web_fetcher,
+                )
+                .await?;
+                Exporter::export_page(&subtask_name, html, &target_dir).await?;
             }
             let _ = self.task_status.try_write().map(|mut op| {
                 let progress = (posts_sum - local_posts.len()) as f32 / posts_sum as f32;
@@ -226,8 +214,7 @@ impl TaskHandler {
 
         for (i, page) in range.into_iter().enumerate() {
             let posts_sum = self
-                .processer
-                .download_fav_posts(self.uid, page, with_pic, image_definition)
+                .backup_fav_post(self.uid, page, with_pic, image_definition)
                 .await?;
             total_downloaded += posts_sum;
             info!("fetched {} posts in {}th page", posts_sum, page);
@@ -269,12 +256,83 @@ impl TaskHandler {
         db_total: &mut u32,
     ) -> Result<()> {
         result?;
-        let (web_total_res, db_total_res) = tokio::join!(
-            self.processer.get_web_total_num(),
-            self.processer.get_db_total_num()
-        );
+        let (web_total_res, db_total_res) =
+            tokio::join!(self.get_web_total_num(), self.get_db_total_num());
         *web_total = web_total_res?;
         *db_total = db_total_res?;
         Ok(())
+    }
+
+    pub async fn backup_fav_post(
+        &self,
+        uid: i64,
+        page: u32,
+        with_pic: bool,
+        image_definition: u8,
+    ) -> Result<usize> {
+        let posts = Post::fetch_fav_posts(uid, page, &self.web_fetcher).await?;
+        let result = posts.len();
+        let ids = posts.iter().map(|post| post.id).collect::<Vec<_>>();
+        Post::persist_posts(
+            posts,
+            with_pic,
+            image_definition,
+            self.persister.db().unwrap(),
+            &self.web_fetcher,
+        )
+        .await?;
+
+        // call mark_user_backed_up after all posts inserted, to ensure the post is in db
+        join_all(
+            ids.into_iter()
+                .map(|id| Post::mark_post_favorited(id, self.persister.db().unwrap())),
+        )
+        .await
+        .into_iter()
+        .collect::<Result<_>>()?;
+
+        Ok(result)
+    }
+
+    pub async fn load_fav_posts_from_db(
+        &self,
+        range: RangeInclusive<u32>,
+        reverse: bool,
+    ) -> Result<Vec<Post>> {
+        let limit = (range.end() - range.start()) + 1;
+        let offset = *range.start() - 1;
+        Post::query_posts(limit, offset, reverse, self.persister.db().unwrap()).await
+    }
+
+    pub async fn backup_ones_posts(
+        &self,
+        uid: i64,
+        page: u32,
+        with_pic: bool,
+        image_definition: u8,
+    ) -> Result<usize> {
+        let posts = Post::fetch_posts(uid, page, &self.web_fetcher).await?;
+        let result = posts.len();
+        Post::persist_posts(
+            posts,
+            with_pic,
+            image_definition,
+            self.persister.db().unwrap(),
+            &self.web_fetcher,
+        )
+        .await?;
+        // mark_user_backed_up should be called after all posts inserted,
+        // to ensure the user info is persisted
+        User::mark_user_backed_up(uid, self.persister.db().unwrap()).await?;
+
+        Ok(result)
+    }
+
+    async fn get_web_total_num(&self) -> Result<u32> {
+        self.web_fetcher.fetch_fav_total_num().await
+    }
+
+    async fn get_db_total_num(&self) -> Result<u32> {
+        Post::query_favorited_sum(self.persister.db().unwrap()).await
     }
 }
