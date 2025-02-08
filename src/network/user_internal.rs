6b86b703f617b3e39b164d493dcd04c91aa1fb9b
Shapooo <ShaboJia@outlook.com>
2025-02-08 15:55:08 +0800

    移除 network::user_internal::UserInternal 里不必要的宏
    Former-commit-id: f7936f3dd3aab71185a20b6e3f064c03ed941fea

--- src/network/user_internal.rs
+++ src/network/user_internal.rs
@@ -1,14 +1,10 @@
-use std::ops::DerefMut;
-
-use anyhow::{Error, Result};
-use log::{debug, trace};
+use anyhow::Error;
 use serde::{Deserialize, Serialize};
-use serde_json::{to_string, Value};
-use sqlx::{Executor, FromRow, Sqlite};
+use serde_json::Value;
 
 const USER_INFO_API: &str = "https://weibo.com/ajax/profile/info";
 
-#[derive(Deserialize, Serialize, Debug, Clone, FromRow, PartialEq)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct UserInternal {
     #[serde(default)]
     pub id: i64,
@@ -21,48 +17,35 @@ pub struct UserInternal {
     pub avatar_large: String,
     #[serde(default)]
     pub avatar_hd: String,
-    #[sqlx(default)]
     #[serde(default)]
     pub planet_video: bool,
-    #[sqlx(default)]
     #[serde(default, deserialize_with = "parse_v_plus")]
     pub v_plus: i64,
-    #[sqlx(default)]
     #[serde(default)]
     pub pc_new: i64,
-    #[sqlx(default)]
     #[serde(default)]
     pub verified: bool,
-    #[sqlx(default)]
     #[serde(default)]
     pub verified_type: i64,
-    #[sqlx(default)]
     #[serde(default)]
     pub domain: String,
-    #[sqlx(default)]
     #[serde(default)]
     pub weihao: String,
-    #[sqlx(default)]
     pub verified_type_ext: Option<i64>,
-    #[sqlx(default)]
     #[serde(default)]
     pub follow_me: bool,
-    #[sqlx(default)]
     #[serde(default)]
     pub following: bool,
-    #[sqlx(default)]
     #[serde(default)]
     pub mbrank: i64,
-    #[sqlx(default)]
     #[serde(default)]
     pub mbtype: i64,
     pub icon_list: Option<Value>,
-    #[sqlx(default)]
     #[serde(default)]
     pub backedup: bool,
 }
 
-fn parse_v_plus<'de, D>(deserializer: D) -> std::result::Result<i64, D::Error>
+pub fn parse_v_plus<'de, D>(deserializer: D) -> std::result::Result<i64, D::Error>
 where
     D: serde::Deserializer<'de>,
 {
@@ -83,134 +66,4 @@ impl TryInto<Value> for UserInternal {
     fn try_into(self) -> std::result::Result<Value, Self::Error> {
         serde_json::to_value(self)
     }
-}
-
-impl UserInternal {
-
-    // mark_user_backed_up must be called after all posts inserted,
-    // to ensure the user info is persisted
-    pub async fn mark_user_backed_up<E>(uid: i64, mut db: E) -> Result<()>
-    where
-        E: DerefMut,
-        for<'a> &'a mut E::Target: Executor<'a, Database = Sqlite>,
-    {
-        debug!("mark user {} backedup", uid);
-        sqlx::query("UPDATE users SET backedup = true WHERE id = ?")
-            .bind(uid)
-            .execute(&mut *db)
-            .await?;
-        Ok(())
-    }
-
-    pub fn get_avatar_pic(&self, image_definition: u8) -> Picture {
-        match image_definition {
-            0 => Picture::avatar(self.profile_image_url.as_str(), self.id),
-            1 => Picture::avatar(self.avatar_large.as_str(), self.id),
-            2 => Picture::avatar(self.avatar_hd.as_str(), self.id),
-            _ => unreachable!(),
-        }
-    }
-
-    pub fn get_download_url(id: i64) -> String {
-        format!("{}?uid={}", USER_INFO_API, id)
-    }
-}
-
-#[cfg(test)]
-mod user_test {
-    use super::UserInternal;
-    use anyhow::Result;
-    use flate2::read::GzDecoder;
-    use serde_json::{from_str, Value};
-    use std::collections::HashMap;
-    use std::io::Read;
-
-    async fn create_db() -> Result<sqlx::SqlitePool> {
-        Ok(sqlx::sqlite::SqlitePoolOptions::new()
-            .connect("sqlite::memory:")
-            .await?)
-    }
-
-    async fn load_test_case() -> Result<Vec<Value>> {
-        let gz = include_bytes!("../../../res/full.json.gz");
-        let mut de = GzDecoder::new(gz.as_ref());
-        let mut text = String::new();
-        de.read_to_string(&mut text)?;
-
-        let test_case_post: Vec<Value> = from_str(&text)?;
-        let test_case = test_case_post
-            .into_iter()
-            .filter_map(|mut v| v["user"].is_object().then_some(v["user"].take()))
-            .collect();
-        Ok(test_case)
-    }
-
-    #[tokio::test]
-    async fn create_table() {
-        let db = create_db().await.unwrap();
-        let conn = db.acquire().await.unwrap();
-        UserInternal::create_table(conn).await.unwrap();
-    }
-
-    async fn parse_users(test_case: Vec<Value>) -> Result<Vec<UserInternal>> {
-        test_case
-            .into_iter()
-            .map(|user| {
-                let user: UserInternal = user.try_into()?;
-                Ok(user)
-            })
-            .collect::<Result<Vec<_>>>()
-    }
-
-    #[tokio::test]
-    async fn parse_from_json() {
-        let test_case = load_test_case().await.unwrap();
-        parse_users(test_case).await.unwrap();
-    }
-
-    // #[tokio::test]
-    // async fn insert() {
-    //     let db = create_db().await.unwrap();
-    //     let mut trans = db.begin().await.unwrap();
-    //     User::create_table(trans.as_mut()).await.unwrap();
-    //     let test_case = load_test_case().await.unwrap();
-    //     let test_case = parse_users(test_case).await.unwrap();
-    //     let test_case = test_case
-    //         .into_iter()
-    //         .filter(|user| user.id != i64::default())
-    //         .collect::<Vec<_>>();
-    //     for user in test_case {
-    //         user.insert(trans.as_mut()).await.unwrap();
-    //     }
-    //     trans.commit().await.unwrap();
-    // }
-
-    // #[tokio::test]
-    // async fn query() {
-    //     let ref db = create_db().await.unwrap();
-    //     let mut trans = db.begin().await.unwrap();
-    //     User::create_table(trans.as_mut()).await.unwrap();
-    //     let test_case = load_test_case().await.unwrap();
-    //     let test_case = parse_users(test_case).await.unwrap();
-    //     let test_case = test_case
-    //         .into_iter()
-    //         .filter_map(|user| (user.id != i64::default()).then_some((user.id, user)))
-    //         .collect::<HashMap<_, _>>();
-    //     for user in test_case.values() {
-    //         user.insert(trans.as_mut()).await.unwrap();
-    //     }
-    //     let mut queried_user = Vec::new();
-    //     for &id in test_case.keys() {
-    //         queried_user.push(User::query(id, trans.as_mut()).await.unwrap());
-    //     }
-    //     let queried_user = queried_user
-    //         .into_iter()
-    //         .filter_map(|user| user)
-    //         .collect::<Vec<_>>();
-    //     assert_eq!(queried_user.len(), test_case.len());
-    //     queried_user.into_iter().for_each(|user| {
-    //         let origin = test_case.get(&user.id).unwrap();
-    //         assert_eq!(origin, &user);
-    //     });
-    // }
 }
