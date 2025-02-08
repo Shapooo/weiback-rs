3d40d70dc82aad33c5ee0c7d7eacc79ad233ada7
Shapooo <ShaboJia@outlook.com>
2025-02-08 15:54:28 +0800

    添加 network/poicture_internal 模拟用于处理图片下载

--- src/network/picture_internal.rs
+++ src/network/picture_internal.rs
@@ -1,18 +1,22 @@
 use anyhow::Result;
 use log::{debug, trace};
 
-use super::NetworkImpl;
-use crate::app::models::Picture;
+use super::HttpClient;
+use crate::models::Picture;
 
-struct PictureClient<'a> {
-    network: &'a NetworkImpl,
+pub struct PictureClient {
+    http_client: HttpClient,
 }
 
-impl<'a> PictureClient<'a> {
-    async fn fetch_blob(&self, pic: &mut Picture) -> Result<()> {
+impl PictureClient {
+    pub fn new(http_client: HttpClient) -> Self {
+        Self { http_client }
+    }
+
+    pub async fn get_blob(&self, pic: &mut Picture) -> Result<()> {
         let url = pic.get_url();
         debug!("fetch pic, url: {}", url);
-        let res = self.network.get(url).await?;
+        let res = self.http_client.get(url).await?;
         let res_bytes = res.bytes().await?;
         trace!("fetched pic size: {}", res_bytes.len());
         pic.set_blob(res_bytes);
