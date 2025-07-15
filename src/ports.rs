c2cd6de9d013344ba30219fb1bf85b74e5f4ec6a
Shapooo <ShaboJia@outlook.com>
2025-07-15 11:03:09 +0800

    创建任务选项 TaskOptions 方便进行参数传递

--- src/ports.rs
+++ src/ports.rs
@@ -1,3 +1,4 @@
+#![allow(async_fn_in_trait)]
 use std::sync::Arc;
 use std::{ops::RangeInclusive, path::Path};
 
@@ -11,15 +12,15 @@ use super::models::Picture;
 #[derive(Debug, Clone)]
 pub enum Task {
     // to fetch user meta data, include screen name and avatar
-    FetchUserMeta(i64),
+    FetchUserMeta(TaskOptions),
     // to download favorites (range, with pic, image definition level)
-    BackupFavorites(RangeInclusive<u32>, bool, u8),
+    BackupFavorites(TaskOptions),
     // to export favorites from local db (range, with pic, image definition level)
-    ExportFromLocal(RangeInclusive<u32>, bool, u8),
+    ExportFromLocal(TaskOptions),
     // to unfavorite favorite post
     UnfavoritePosts,
     // to backup user (id, with pic, image definition level)
-    BackupUser(i64, bool, u8),
+    BackupUser(TaskOptions),
 }
 
 #[allow(unused)]
@@ -33,29 +34,19 @@ pub enum TaskResponse {
 
 pub trait Service {
     async fn unfavorite_posts(&self);
-    async fn backup_favorites(
-        &self,
-        range: RangeInclusive<u32>,
-        with_pic: bool,
-        image_definition: u8,
-    );
-    async fn backup_user(&self, uid: i64, with_pic: bool, image_definition: u8);
-    async fn export_from_local(
-        &self,
-        range: RangeInclusive<u32>,
-        reverse: bool,
-        image_definition: u8,
-    );
+    async fn backup_favorites(&self, options: TaskOptions);
+    async fn backup_user(&self, options: TaskOptions);
+    async fn export_from_local(&self, options: TaskOptions);
 }
 
 pub trait Storage {
     async fn save_user(&self, user: &User) -> Result<()>;
-    async fn get_user(&self, id: i64) -> Result<Option<User>>;
-    async fn get_posts(&self, limit: u32, offset: u32, reverse: bool) -> Result<Vec<Post>>;
+    async fn get_user(&self, options: TaskOptions) -> Result<Option<User>>;
+    async fn get_posts(&self, options: TaskOptions) -> Result<Vec<Post>>;
     async fn save_post(&self, post: &Post) -> Result<()>;
-    async fn get_post(&self, id: i64) -> Result<Option<Post>>;
-    async fn mark_post_unfavorited(&self, id: i64) -> Result<()>;
-    async fn mark_post_favorited(&self, id: i64) -> Result<()>;
+    async fn get_post(&self, options: TaskOptions) -> Result<Option<Post>>;
+    async fn mark_post_unfavorited(&self, options: TaskOptions) -> Result<()>;
+    async fn mark_post_favorited(&self, options: TaskOptions) -> Result<()>;
     async fn get_favorited_sum(&self) -> Result<u32>;
     async fn get_posts_id_to_unfavorite(&self) -> Result<Vec<i64>>;
     async fn save_picture(&self, picture: &Picture) -> Result<()>;
@@ -66,15 +57,15 @@ impl<S: Storage> Storage for Arc<S> {
         self.as_ref().save_user(user).await
     }
 
-    async fn get_user(&self, id: i64) -> Result<Option<User>> {
-        self.as_ref().get_user(id).await
+    async fn get_user(&self, options: TaskOptions) -> Result<Option<User>> {
+        self.as_ref().get_user(options).await
     }
-    async fn get_post(&self, id: i64) -> Result<Option<Post>> {
-        self.as_ref().get_post(id).await
+    async fn get_post(&self, options: TaskOptions) -> Result<Option<Post>> {
+        self.as_ref().get_post(options).await
     }
 
-    async fn get_posts(&self, limit: u32, offset: u32, reverse: bool) -> Result<Vec<Post>> {
-        self.as_ref().get_posts(limit, offset, reverse).await
+    async fn get_posts(&self, options: TaskOptions) -> Result<Vec<Post>> {
+        self.as_ref().get_posts(options).await
     }
 
     async fn save_post(&self, post: &Post) -> Result<()> {
@@ -89,12 +80,12 @@ impl<S: Storage> Storage for Arc<S> {
         self.as_ref().get_posts_id_to_unfavorite().await
     }
 
-    async fn mark_post_favorited(&self, id: i64) -> Result<()> {
-        self.as_ref().mark_post_favorited(id).await
+    async fn mark_post_favorited(&self, options: TaskOptions) -> Result<()> {
+        self.as_ref().mark_post_favorited(options).await
     }
 
-    async fn mark_post_unfavorited(&self, id: i64) -> Result<()> {
-        self.as_ref().mark_post_unfavorited(id).await
+    async fn mark_post_unfavorited(&self, options: TaskOptions) -> Result<()> {
+        self.as_ref().mark_post_unfavorited(options).await
     }
     async fn save_picture(&self, picture: &Picture) -> Result<()> {
         self.as_ref().save_picture(picture).await
@@ -112,4 +103,76 @@ pub trait Exporter: 'static + Clone + Send + Sync {
 
 pub trait Processer: 'static + Clone + Send + Sync {
     async fn generate_html(&self) -> Result<String>;
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub enum UserPostFilter {
+    #[default]
+    All,
+    Original,
+    Video,
+    Picture,
+}
+
+#[derive(Debug, Clone)]
+pub struct TaskOptions {
+    pub with_pic: bool,
+    pub post_id: i64,
+    pub uid: i64,
+    pub reverse: bool,
+    pub range: Option<RangeInclusive<u32>>,
+    pub post_filter: UserPostFilter,
+}
+
+impl Default for TaskOptions {
+    fn default() -> Self {
+        Self {
+            with_pic: false,
+            post_id: 0,
+            uid: 0,
+            reverse: false,
+            range: None,
+            post_filter: UserPostFilter::default(),
+        }
+    }
+}
+
+impl TaskOptions {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with_pic(mut self) -> Self {
+        self.with_pic = true;
+        self
+    }
+
+    pub fn with_user(mut self, uid: i64) -> Self {
+        self.uid = uid;
+        self
+    }
+
+    pub fn with_post(mut self, post_id: i64) -> Self {
+        self.post_id = post_id;
+        self
+    }
+
+        self.pic_quality = quality;
+        self
+    }
+
+    pub fn reverse(mut self) -> Self {
+        self.reverse = true;
+        self
+    }
+
+    pub fn range(mut self, range: RangeInclusive<u32>) -> Self {
+        self.range = Some(range);
+        self
+    }
+
+    pub fn post_filter(mut self, filter: UserPostFilter) -> Self {
+        self.post_filter = filter;
+        self
+    }
 }
