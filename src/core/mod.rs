004656998808f5552ce14ae49c271b55884c1478
Shapooo <ShaboJia@outlook.com>
2025-07-22 21:13:40 +0800

    粗略将 ui 中逻辑部分移到 core 模块，只留界面显示
    Former-commit-id: 9e78157bedd8fe9990069f64eef28389ca4b451e

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,6 +2,107 @@ pub mod options;
 pub mod task_handler;
 pub mod task_proxy;
 
+use log::info;
+use tokio::sync::mpsc::{Receiver, channel, error::TryRecvError};
+
+use crate::error::Result;
+use crate::message::Message;
+use crate::ui::{
+    AsAny, UI,
+    tabs::{backup_fav_tab::BackupFavTab, export_from_local_tab::ExportFromLocalTab},
+};
+
 pub use options::{TaskOptions, UserPostFilter};
 pub use task_handler::{Task, TaskHandler};
 pub use task_proxy::TaskProxy;
+
+pub struct Core {
+    task_executor: TaskProxy,
+    msg_receiver: Receiver<Result<Message>>,
+    ui: UI,
+}
+
+impl Core {
+    pub fn new() -> Self {
+        let (msg_sender, msg_receiver) = channel(100);
+        Self {
+            task_executor: TaskProxy::new(msg_sender),
+            msg_receiver,
+            ui: UI::new(),
+        }
+    }
+
+    pub fn run(self) -> Result<()> {
+        info!("starting gui...");
+        eframe::run_native(
+            "weiback",
+            UI::default_options(),
+            Box::new(|cc| {
+                self.ui.set_context(cc);
+                Ok(Box::new(self))
+            }),
+        )
+        .unwrap();
+        Ok(())
+    }
+
+    fn handle_task_responses(&mut self) {
+        let task_status: Option<Result<Message>> = match self.msg_receiver.try_recv() {
+            Ok(status) => Some(status),
+            Err(TryRecvError::Empty) => None,
+            Err(e) => panic!("{}", e),
+        };
+
+        if let Some(task_status) = task_status {
+            match task_status {
+                Ok(Message::SumOfFavDB(web_total, db_total)) => {
+                    self.ui.web_total = web_total;
+                    self.ui.db_total = db_total;
+                    if let Some(tab) = self.ui.tabs[0].as_any_mut().downcast_mut::<BackupFavTab>() {
+                        tab.set_web_total(web_total);
+                    }
+                    if let Some(tab) = self.ui.tabs[2]
+                        .as_any_mut()
+                        .downcast_mut::<ExportFromLocalTab>()
+                    {
+                        tab.set_db_total(db_total);
+                    }
+                    self.ui.message = format!(
+                        "账号共 {} 条收藏\n本地保存有 {} 条收藏",
+                        self.ui.web_total, self.ui.db_total
+                    );
+                }
+                Ok(Message::InProgress(ratio, msg)) => {
+                    self.ui.ratio = ratio;
+                    self.ui.message = msg;
+                }
+                Ok(Message::Finished(web_total, db_total)) => {
+                    self.ui.ratio = 1.;
+                    self.ui.task_ongoing = false;
+                    self.ui.web_total = web_total;
+                    self.ui.db_total = db_total;
+                    self.ui.message = format!(
+                        "任务完成!\n账号剩 {} 条收藏\n本地保存有 {} 条收藏",
+                        self.ui.web_total, self.ui.db_total
+                    );
+                }
+                Ok(Message::UserMeta(_id, _screen_name, _avatar)) => {
+                    // TODO: how to show user meta?
+                }
+                Err(msg) => {
+                    self.ui.task_ongoing = false;
+                    self.ui.message = msg.to_string();
+                }
+            }
+        }
+    }
+}
+
+impl eframe::App for Core {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        self.handle_task_responses();
+        if let Some(task) = self.ui.update(ctx, frame) {
+            self.task_executor.send_task(task);
+        }
+    }
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,37 +1,27 @@
-mod tabs;
+pub mod tabs;
 
 use std::any::Any;
 use std::time::Duration;
 
-use anyhow;
 use eframe::{
     NativeOptions,
     egui::{vec2, viewport::ViewportBuilder},
 };
-use log::info;
-use tokio::sync::mpsc::{Receiver, channel, error::TryRecvError};
 
-use crate::core::{Task, TaskProxy};
-use crate::error::Result;
-use crate::message::Message;
+use crate::core::Task;
 use tabs::{
     Tab, about_tab::AboutTab, backup_fav_tab::BackupFavTab, backup_user_tab::BackupUserTab,
     export_from_local_tab::ExportFromLocalTab,
 };
 
 pub struct UI {
-    tabs: Vec<Box<dyn Tab>>,
-    current_tab_idx: usize,
-
-    task_status_receiver: Receiver<Result<Message>>,
-    executor: TaskProxy,
-    task_ongoing: bool,
-
-    // View data
-    message: String,
-    ratio: f32,
-    web_total: u32,
-    db_total: u32,
+    pub tabs: Vec<Box<dyn Tab>>,
+    pub current_tab_idx: usize,
+    pub task_ongoing: bool,
+    pub message: String,
+    pub ratio: f32,
+    pub web_total: u32,
+    pub db_total: u32,
 }
 
 impl Default for UI {
@@ -42,12 +32,9 @@ impl Default for UI {
             Box::<ExportFromLocalTab>::default(),
             Box::<AboutTab>::default(),
         ];
-        let (task_status_sender, task_status_receiver) = channel(100);
         Self {
             tabs,
             current_tab_idx: 0,
-            task_status_receiver,
-            executor: TaskProxy::new(task_status_sender),
             task_ongoing: false,
             message: "请开始任务".into(),
             ratio: 0.0,
@@ -62,72 +49,43 @@ impl UI {
         Self::default()
     }
 
-    pub fn run(self) -> anyhow::Result<()> {
-        info!("starting gui...");
-        eframe::run_native(
-            "weiback",
-            NativeOptions {
-                viewport: ViewportBuilder::default().with_inner_size(vec2(330., 300.)),
-                ..Default::default()
-            },
-            Box::new(|cc| {
-                set_font(cc);
-                Ok(Box::new(self))
-            }),
-        )
-        .unwrap();
-        Ok(())
+    pub fn default_options() -> NativeOptions {
+        NativeOptions {
+            viewport: ViewportBuilder::default().with_inner_size(vec2(330., 300.)),
+            ..Default::default()
+        }
     }
 
-    fn handle_task_responses(&mut self) {
-        let task_status: Option<Result<Message>> = match self.task_status_receiver.try_recv() {
-            Ok(status) => Some(status),
-            Err(TryRecvError::Empty) => None,
-            Err(e) => panic!("{}", e),
-        };
+    pub fn set_context(&self, cc: &eframe::CreationContext) {
+        set_font(cc);
+    }
 
-        if let Some(task_status) = task_status {
-            match task_status {
-                Ok(Message::SumOfFavDB(web_total, db_total)) => {
-                    self.web_total = web_total;
-                    self.db_total = db_total;
-                    if let Some(tab) = self.tabs[0].as_any_mut().downcast_mut::<BackupFavTab>() {
-                        tab.set_web_total(web_total);
+    pub fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) -> Option<Task> {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered(|ui| ui.heading("WeiBack"));
+            ui.add_enabled_ui(!self.task_ongoing, |ui| {
+                ui.group(|ui| {
+                    ui.horizontal(|ui| {
+                        for (i, tab) in self.tabs.iter().enumerate() {
+                            ui.selectable_value(&mut self.current_tab_idx, i, tab.name());
+                        }
+                    });
+
+                    let tab = &mut self.tabs[self.current_tab_idx];
+                    if let Some(task) = tab.ui(ui) {
+                        self.task_ongoing = true;
+                        self.ratio = 0.0;
+                        self.message = "任务开始...".into();
                     }
-                    if let Some(tab) = self.tabs[2]
-                        .as_any_mut()
-                        .downcast_mut::<ExportFromLocalTab>()
-                    {
-                        tab.set_db_total(db_total);
-                    }
-                    self.message = format!(
-                        "账号共 {} 条收藏\n本地保存有 {} 条收藏",
-                        self.web_total, self.db_total
-                    );
-                }
-                Ok(Message::InProgress(ratio, msg)) => {
-                    self.ratio = ratio;
-                    self.message = msg;
-                }
-                Ok(Message::Finished(web_total, db_total)) => {
-                    self.ratio = 1.;
-                    self.task_ongoing = false;
-                    self.web_total = web_total;
-                    self.db_total = db_total;
-                    self.message = format!(
-                        "任务完成!\n账号剩 {} 条收藏\n本地保存有 {} 条收藏",
-                        self.web_total, self.db_total
-                    );
-                }
-                Ok(Message::UserMeta(_id, _screen_name, _avatar)) => {
-                    // TODO: how to show user meta?
-                }
-                Err(msg) => {
-                    self.task_ongoing = false;
-                    self.message = msg.to_string();
-                }
-            }
-        }
+                });
+            });
+            ui.add(egui::ProgressBar::new(self.ratio).show_percentage());
+            ui.vertical_centered(|ui| {
+                ui.label(&self.message);
+            });
+        });
+        ctx.request_repaint_after(Duration::from_millis(200));
+        Some(Task::UnfavoritePosts)
     }
 }
 
@@ -146,46 +104,9 @@ fn set_font(cc: &eframe::CreationContext) {
     cc.egui_ctx.set_fonts(fonts);
 }
 
-impl eframe::App for UI {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        self.handle_task_responses();
-        self.when_logined(ctx, frame);
-        ctx.request_repaint_after(Duration::from_millis(200));
-    }
-}
-
-impl UI {
-    fn when_logined(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.vertical_centered(|ui| ui.heading("WeiBack"));
-            ui.add_enabled_ui(!self.task_ongoing, |ui| {
-                ui.group(|ui| {
-                    ui.horizontal(|ui| {
-                        for (i, tab) in self.tabs.iter().enumerate() {
-                            ui.selectable_value(&mut self.current_tab_idx, i, tab.name());
-                        }
-                    });
-
-                    let tab = &mut self.tabs[self.current_tab_idx];
-                    if let Some(task) = tab.ui(ui) {
-                        self.task_ongoing = true;
-                        self.ratio = 0.0;
-                        self.message = "任务开始...".into();
-                        self.executor.send_task(task);
-                    }
-                });
-            });
-            ui.add(egui::ProgressBar::new(self.ratio).show_percentage());
-            ui.vertical_centered(|ui| {
-                ui.label(&self.message);
-            });
-        });
-    }
-}
-
 // Trait object `dyn Tab` doesn't have a constant size known at compile-time,
 // so we need to implement `as_any_mut` to downcast it.
-trait AsAny: 'static {
+pub trait AsAny: 'static {
     fn as_any_mut(&mut self) -> &mut dyn Any;
 }
 
