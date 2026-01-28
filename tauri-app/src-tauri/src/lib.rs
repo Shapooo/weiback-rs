faa855d8a487f2be89530a9a40565e94549b8518
Shapooo <ShaboJia@outlook.com>
2026-01-28 16:05:09 +0800

    更新 tauri app 的 lib.rs ，使用 CoreBuilder 进行初始化

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -1,40 +1,20 @@
 mod error;
 
 use std::ops::RangeInclusive;
+use std::sync::Arc;
 
-use log::{debug, error, info, warn};
+use log::{error, info};
 use serde_json::Value;
 use tauri::{self, App, AppHandle, Emitter, Manager, State};
 use tokio::sync::mpsc;
-
-use weiback::api::ApiClientImpl;
+use weiback::builder::CoreBuilder;
 use weiback::config::{Config, get_config};
 use weiback::core::{
-    BFOptions, BUOptions, Core, ExportOptions, TaskRequest, task_handler::TaskHandler,
-    task_manager::TaskManger,
+    BFOptions, BUOptions, Core, ExportOptions, TaskRequest, task_manager::TaskManger,
 };
-#[cfg(feature = "dev-mode")]
-use weiback::dev_client::DevClient;
-use weiback::exporter::ExporterImpl;
-use weiback::media_downloader::{MediaDownloaderHandle, create_downloader};
 use weiback::message::{ErrMsg, Message, TaskProgress};
-use weiback::models::User;
-use weiback::storage::StorageImpl;
-use weibosdk_rs::{
-    ApiClient as SdkApiClient, Client as HttpClient, api_client::LoginState, session::Session,
-};
 
-use error::{Error, Result};
-
-#[cfg(feature = "dev-mode")]
-type TH = TaskHandler<weiback::api::DevApiClient, StorageImpl, ExporterImpl, MediaDownloaderHandle>;
-#[cfg(not(feature = "dev-mode"))]
-type TH =
-    TaskHandler<weiback::api::DefaultApiClient, StorageImpl, ExporterImpl, MediaDownloaderHandle>;
-#[cfg(feature = "dev-mode")]
-type CurrentSdkApiClient = SdkApiClient<DevClient>;
-#[cfg(not(feature = "dev-mode"))]
-type CurrentSdkApiClient = SdkApiClient<HttpClient>;
+use error::Result;
 
 #[tauri::command]
 fn get_config_command() -> std::result::Result<Config, String> {
@@ -50,23 +30,23 @@ fn set_config_command(config: Config) -> std::result::Result<(), String> {
 }
 
 #[tauri::command]
-async fn backup_self(
-    core: State<'_, Core>,
-    api_client: State<'_, SdkApiClient<HttpClient>>,
-    range: RangeInclusive<u32>,
-) -> Result<()> {
-    info!("backup_self called with range: {range:?}");
-    let uid = api_client.session()?.uid.clone();
+async fn backup_self(core: State<'_, Arc<Core>>, range: RangeInclusive<u32>) -> Result<()> {
+    info!("backup_self called with range: {:?}", range);
+    let uid = core.get_my_uid()?;
     backup_user(core, uid, range).await
 }
 
 #[tauri::command]
-async fn backup_user(core: State<'_, Core>, uid: String, range: RangeInclusive<u32>) -> Result<()> {
-    info!("backup_user called with uid: {uid}, range: {range:?}");
+async fn backup_user(
+    core: State<'_, Arc<Core>>,
+    uid: String,
+    range: RangeInclusive<u32>,
+) -> Result<()> {
+    info!("backup_user called with uid: {}, range: {:?}", uid, range);
     Ok(core
         .backup_user(TaskRequest::BackupUser(BUOptions {
             uid: uid.parse().map_err(|err| {
-                error!("Failed to parse uid: {err}");
+                error!("Failed to parse uid: {}", err);
                 err
             })?,
             range,
@@ -75,112 +55,54 @@ async fn backup_user(core: State<'_, Core>, uid: String, range: RangeInclusive<u
 }
 
 #[tauri::command]
-async fn backup_favorites(core: State<'_, Core>, range: RangeInclusive<u32>) -> Result<()> {
-    info!("backup_favorites called with range: {range:?}");
+async fn backup_favorites(core: State<'_, Arc<Core>>, range: RangeInclusive<u32>) -> Result<()> {
+    info!("backup_favorites called with range: {:?}", range);
     Ok(core
         .backup_favorites(TaskRequest::BackupFavorites(BFOptions { range }))
         .await?)
 }
 
 #[tauri::command]
-async fn unfavorite_posts(core: State<'_, Core>) -> Result<()> {
+async fn unfavorite_posts(core: State<'_, Arc<Core>>) -> Result<()> {
     info!("unfavorite_posts called");
     Ok(core.unfavorite_posts().await?)
 }
 
 #[tauri::command]
-async fn export_from_local(task_handler: State<'_, TH>, range: RangeInclusive<u32>) -> Result<()> {
-    info!("export_from_local called with range: {range:?}");
+async fn export_from_local(core: State<'_, Arc<Core>>, range: RangeInclusive<u32>) -> Result<()> {
+    info!("export_from_local called with range: {:?}", range);
     let options = ExportOptions {
         range,
         ..Default::default()
     };
-    Ok(task_handler.export_from_local(options).await?)
+    Ok(core.export_from_local(options).await?)
 }
 
 #[tauri::command]
-async fn get_sms_code(
-    api_client: State<'_, CurrentSdkApiClient>,
-    phone_number: String,
-) -> Result<()> {
-    info!(
-        "send_code called for phone number (partially hidden): ...{}",
-        &phone_number.chars().skip(7).collect::<String>()
-    );
-    match api_client.login_state() {
-        LoginState::LoggedIn { .. } => {
-            warn!("Already logged in, skipping send_code.");
-            Ok(())
-        }
-        _ => {
-            info!("Sending SMS code.");
-            api_client.get_sms_code(phone_number).await?;
-            Ok(())
-        }
-    }
+async fn get_sms_code(core: State<'_, Arc<Core>>, phone_number: String) -> Result<()> {
+    Ok(core.get_sms_code(phone_number).await?)
 }
 
 #[tauri::command]
-async fn login(
-    api_client: State<'_, SdkApiClient<HttpClient>>,
-    task_handler: State<'_, TH>,
-    sms_code: String,
-) -> Result<()> {
-    info!("login called with a sms_code");
-    match api_client.login_state() {
-        LoginState::WaitingForCode { .. } => {
-            info!("Attempting to login with SMS code.");
-            api_client.login(&sms_code).await?;
-            info!("Login successful.");
-            let session_path = get_config()
-                .read()
-                .expect("config lock failed")
-                .session_path
-                .clone();
-            let session = api_client.session()?;
-            session.save(session_path)?;
-
-            let user: User = serde_json::from_value(session.user)?;
-            let user_id = user.id;
-            let th = task_handler.inner().clone();
-            tauri::async_runtime::spawn(async move {
-                if let Err(e) = th.save_user_info(&user).await {
-                    error!("Save user info failed: {e}");
-                }
-            });
-            info!("Logged in user {} saved.", user_id);
-
-            Ok(())
-        }
-        LoginState::LoggedIn { .. } => {
-            warn!("Already logged in, skipping login.");
-            Ok(())
-        }
-        LoginState::Init => {
-            error!("Wrong login state to login: Init");
-            Err(Error("FATAL: wrong login state to login".to_string()))
-        }
-    }
+async fn login(core: State<'_, Arc<Core>>, sms_code: String) -> Result<()> {
+    Ok(core.login(sms_code).await?)
 }
 
 #[tauri::command]
-async fn login_state(api_client: State<'_, CurrentSdkApiClient>) -> Result<Option<Value>> {
-    info!("get login state");
-    Ok(api_client.session().ok().map(|s| s.user))
+async fn login_state(core: State<'_, Arc<Core>>) -> Result<Option<Value>> {
+    Ok(core.login_state().await?)
 }
 
 #[tauri::command]
 async fn get_username_by_id(
-    task_handler: State<'_, TH>,
+    core: State<'_, Arc<Core>>,
     uid: String,
 ) -> std::result::Result<Option<String>, String> {
     let Ok(uid) = uid.parse::<i64>() else {
         return Ok(None);
     };
-    task_handler
-        .get_user(uid)
+    core.get_username_by_id(uid)
         .await
-        .map(|opt_user| opt_user.map(|user| user.screen_name))
         .map_err(|e| e.to_string())
 }
 
@@ -221,74 +143,19 @@ pub fn run() -> Result<()> {
 }
 
 fn setup(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let main_config = get_config();
-    let main_config = main_config.read().map_err(|e| {
-        error!("Failed to read config: {e}");
-        anyhow::anyhow!("{e}")
-    })?;
-    debug!("Weibo API config loaded: {main_config:?}");
-
-    let (msg_sender, msg_receiver) = mpsc::channel(100);
-    info!("MPSC channel created");
-
-    let storage = StorageImpl::new()?;
-    info!("Storage initialized");
-
-    let exporter = ExporterImpl::new();
-    info!("Exporter initialized");
-
-    let http_client = HttpClient::new()?;
-    info!("HTTP client created");
-
-    let (handle, worker) =
-        create_downloader(100, http_client.main_client().clone(), msg_sender.clone());
-    tauri::async_runtime::spawn(worker.run());
-    info!("MediaDownloader initialized");
-
-    #[cfg(feature = "dev-mode")]
-    let dev_client = DevClient::new(http_client.clone(), main_config.dev_mode_out_dir.clone());
-    #[cfg(feature = "dev-mode")]
-    let sdk_api_client = SdkApiClient::new(dev_client, main_config.sdk_config.clone());
-    #[cfg(not(feature = "dev-mode"))]
-    let sdk_api_client = SdkApiClient::new(http_client.clone(), main_config.sdk_config.clone());
-    info!("WeiboAPIImpl initialized");
-
-    let api_client = ApiClientImpl::new(sdk_api_client.clone());
-
-    let task_handler = TaskHandler::new(api_client, storage, exporter, handle, msg_sender)?;
-    info!("TaskHandler initialized");
-
-    let core = Core::new(task_handler.clone())?;
-    info!("Core initialized");
+    let (core, msg_receiver) = CoreBuilder::new().build()?;
 
     info!("Setting up Tauri application");
     tauri::async_runtime::spawn(msg_loop(
         app.handle().clone(),
-        core.task_manager().clone(),
+        core.task_manager(),
         msg_receiver,
     ));
-    app.manage(core);
-    app.manage(task_handler);
-    app.manage(sdk_api_client.clone());
 
-    let session_path = main_config.session_path.clone();
-    if let Ok(session) = Session::load(session_path.as_path()) {
-        tauri::async_runtime::spawn(async move {
-            if let Err(e) = sdk_api_client.login_with_session(session).await {
-                error!("login with session failed: {e}");
-            }
-            info!("login with session successfully");
-            let Ok(session) = sdk_api_client
-                .session()
-                .map_err(|e| error!("get new session failed: {e}"))
-            else {
-                return;
-            };
-            if let Err(e) = session.save(session_path) {
-                error!("save new session failed: {e}");
-            }
-        });
-    }
+    let core_clone = core.clone();
+    tauri::async_runtime::spawn(async move { core_clone.login_with_session().await });
+
+    app.manage(core);
 
     info!("Tauri setup complete");
     Ok(())
@@ -296,20 +163,19 @@ fn setup(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
 
 async fn msg_loop(
     app: AppHandle,
-    task_manager: TaskManger,
+    task_manager: Arc<TaskManger>,
     mut msg_receiver: mpsc::Receiver<Message>,
 ) {
     info!("Message loop started");
     loop {
         tokio::select! {
             Some(msg) = msg_receiver.recv() => {
-                debug!("Received message: {msg:?}");
                 if let Err(e) = handle_task_responses(&app, &task_manager, msg).await {
-                    error!("Error handling task response: {e:?}");
+                    error!("Error handling task response: {:?}", e);
                 }
             }
             else => {
-                warn!("Message channel closed, exiting message loop.");
+                info!("Message channel closed, exiting message loop.");
                 break;
             }
         }
@@ -328,7 +194,6 @@ async fn handle_task_responses(
             total_increment,
             progress_increment,
         }) => {
-            debug!("Handling TaskProgress message for task_id: {task_id}");
             let (progress_new, total_new) =
                 task_manager.update_progress(task_id, progress_increment, total_increment)?;
 
@@ -346,7 +211,7 @@ async fn handle_task_responses(
             err,
             r#type,
         }) => {
-            error!("Handling ErrMsg for task_id: {task_id}, type: {type:?}, error: {err}",);
+            error!("Handling ErrMsg for task_id: {task_id}, type: {type:?}, error: {err}");
             app.emit(
                 "error",
                 serde_json::json!({
