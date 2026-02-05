fc39fdba7c3d77282eb0f93b6d517eeb165836a7
Shapooo <ShaboJia@outlook.com>
2026-02-05 22:00:09 +0800

    删除 post.html, 使用 posts.html 批量处理 html 渲染

--- weiback/src/html_generator.rs
+++ weiback/src/html_generator.rs
@@ -1,13 +1,11 @@
 pub mod view_model;
 
-use std::collections::HashMap;
 use std::sync::Arc;
 
 use futures::stream::{self, StreamExt};
 use lazy_static::lazy_static;
 use log::{debug, info};
 use tera::{Context, Tera};
-use url::Url;
 
 use crate::api::EmojiUpdateApi;
 use crate::core::task::TaskContext;
@@ -23,8 +21,6 @@ lazy_static! {
     pub static ref TEMPLATES: Tera = {
         let mut tera = Tera::default();
         tera.add_raw_template("page.html", include_str!("../templates/page.html"))
-            .unwrap();
-        tera.add_raw_template("post.html", include_str!("../templates/post.html"))
             .unwrap();
         tera.add_raw_template("posts.html", include_str!("../templates/posts.html"))
             .unwrap();
@@ -44,21 +40,6 @@ impl<E: EmojiUpdateApi, S: Storage> HTMLGenerator<E, S> {
         Self { storage, emoji_map }
     }
 
-    fn generate_post(
-        &self,
-        post: Post,
-        page_name: &str,
-        emoji_map: Option<&HashMap<String, Url>>,
-        definition: PictureDefinition,
-    ) -> Result<String> {
-        let pic_folder = make_resource_dir_name(page_name);
-        let post_view = PostView::from_post(post, &pic_folder, definition, emoji_map)?;
-
-        let context = Context::from_serialize(post_view)?;
-        let html = TEMPLATES.render("post.html", &context)?;
-        Ok(html)
-    }
-
     async fn generate_page(
         &self,
         posts: Vec<Post>,
@@ -67,14 +48,19 @@ impl<E: EmojiUpdateApi, S: Storage> HTMLGenerator<E, S> {
     ) -> Result<String> {
         let emoji_map = self.emoji_map.get_or_try_init().await.ok();
         info!("Generating page for {} posts", posts.len());
-        let posts_html = posts
+        let pic_folder = make_resource_dir_name(page_name);
+        let post_views: Vec<PostView> = posts
             .into_iter()
-            .map(|p| self.generate_post(p, page_name, emoji_map, pic_quality))
+            .map(|p| PostView::from_post(p, &pic_folder, pic_quality, emoji_map))
             .collect::<Result<Vec<_>>>()?;
-        let posts_html = posts_html.join("");
-        let mut context = Context::new();
-        context.insert("html", &posts_html);
-        let html = TEMPLATES.render("page.html", &context)?;
+
+        let mut posts_context = Context::new();
+        posts_context.insert("posts", &post_views);
+        let posts_html = TEMPLATES.render("posts.html", &posts_context)?;
+
+        let mut page_context = Context::new();
+        page_context.insert("html", &posts_html);
+        let html = TEMPLATES.render("page.html", &page_context)?;
         info!("Successfully generated page");
         Ok(html)
     }
@@ -201,36 +187,6 @@ mod local_tests {
         let mut posts = api.favorites(0).await.unwrap();
         posts.extend(api.profile_statuses(1786055427, 0).await.unwrap());
         posts
-    }
-
-    #[tokio::test]
-    async fn test_generate_post_with_valid_emoji() {
-        let client = create_mock_client();
-        let api = create_mock_api(&client);
-        let posts = create_posts(&api).await;
-        let generator = create_generator(&api).await;
-        let emoji_map = api.emoji_update().await.unwrap();
-        let definition = PictureDefinition::Original;
-
-        for post in posts {
-            generator
-                .generate_post(post, "test", Some(&emoji_map), definition)
-                .unwrap();
-        }
-    }
-
-    #[tokio::test]
-    async fn test_generate_post_with_invalid_emoji() {
-        let client = create_mock_client();
-        let api = create_mock_api(&client);
-        let posts = create_posts(&api).await;
-        let definition = PictureDefinition::Original;
-        let generator = create_generator(&api).await;
-        for post in posts {
-            generator
-                .generate_post(post, "test", None, definition)
-                .unwrap();
-        }
     }
 
     #[tokio::test]
