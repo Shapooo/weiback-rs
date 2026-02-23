7eb37d7f54053110e184b919606f081b942dd51f
Shapooo <ShaboJia@outlook.com>
2026-02-23 18:22:23 +0800

    为前端 snackbar 添加关闭按钮

--- tauri-app/src/main.tsx
+++ tauri-app/src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
+import { SnackbarProvider, useSnackbar, SnackbarKey } from 'notistack';
 import CssBaseline from '@mui/material/CssBaseline';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import { AppThemeProvider } from './ThemeContext';
 import App from './App';
 
@@ -11,11 +13,24 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const SnackbarAction = ({ id }: { id: SnackbarKey }) => {
+  const { closeSnackbar } = useSnackbar();
+  return (
+    <IconButton size="small" aria-label="close" color="inherit" onClick={() => closeSnackbar(id)}>
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppThemeProvider>
       <CssBaseline />
-      <SnackbarProvider maxSnack={3} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        action={(snackbarId) => <SnackbarAction id={snackbarId} />}
+      >
         <BrowserRouter>
           <App />
         </BrowserRouter>
