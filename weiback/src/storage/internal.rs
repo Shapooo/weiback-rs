f878287ec907b0721ee8cb90cfad483e0896f5a0
Shapooo <ShaboJia@outlook.com>
2026-03-02 16:33:38 +0800

    为 storage::internal 添加简单文档说明

--- weiback/src/storage/internal.rs
+++ weiback/src/storage/internal.rs
@@ -1,3 +1,6 @@
+//! This module serves as a container for internal storage logic,
+//! re-exporting sub-modules that handle specific data types like pictures, posts, users, and videos.
+
 pub mod picture;
 pub mod post;
 pub mod user;
